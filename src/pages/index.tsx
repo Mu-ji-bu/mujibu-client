5533c87c332faf8a8748b107ef13149b88bc4e45
Steventhibe <steventhibe@gmail.com>
2023-05-03 23:59:33 +0800

    feat: 將首頁加入 Seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
-import Head from "next/head";
-
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Seo from "@/components/Seo";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Mujibu | Home</title>
-        <meta name="keywords" content="mujibu"></meta>
-      </Head>
+      <Seo templateTitle="Home" />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>木質部，募質部。</h1>
       </main>
