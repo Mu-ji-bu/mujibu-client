b397d72c5c0caf6806cd3ca4b618ab419029eb84
KaiYun Cheng <tanyacheng@gmail.com>
2023-05-13 18:43:53 +0800

    feat: add signup layout

--- src/pages/signup-layout/index.tsx
+++ src/pages/signup-layout/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Container,
   Box,
@@ -12,25 +13,44 @@ import {
 } from '@mui/material';
 import { Google } from '@mui/icons-material';
 
-const LogIn = () => {
+const SignUp = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
   };
 
   return (
     <Container component="div" maxWidth="sm">
       <Box className="border border-solid border-secondary-10 shadow-md my-20 py-10 px-20 flex flex-col items-center rounded-md">
         <Typography className="text-secondary mb-5" component="h2" variant="h5">
-          開始你的募資旅程
+          立即加入會員
         </Typography>
         <Box className="mb-10" component="form" onSubmit={handleSubmit}>
           <TextField
-            margin="normal"
+            margin="dense"
+            required
+            fullWidth
+            id="userName"
+            label="姓名"
+            name="userName"
+            autoComplete="userName"
+            autoFocus
+            size="small"
+          />
+          <TextField
+            margin="dense"
+            required
+            fullWidth
+            id="nickname"
+            label="暱稱"
+            name="nickname"
+            autoComplete="nickname"
+            autoFocus
+            size="small"
+          />
+
+          <TextField
+            margin="dense"
             required
             fullWidth
             id="email"
@@ -38,29 +58,54 @@ const LogIn = () => {
             name="email"
             autoComplete="email"
             autoFocus
+            size="small"
           />
           <TextField
-            className="mb-5"
-            margin="normal"
+            margin="dense"
+            required
+            fullWidth
+            id="gender"
+            label="性別"
+            name="gender"
+            autoComplete="gender"
+            autoFocus
+            size="small"
+          />
+
+          <TextField
+            margin="dense"
             required
             fullWidth
             name="password"
             label="密碼"
             type="password"
             id="password"
-            autoComplete="current-password"
+            autoComplete="passwordCurrent"
+            size="small"
+          />
+          <TextField
+            className="mb-5"
+            margin="dense"
+            required
+            fullWidth
+            name="passwordConfirm"
+            label="再次輸入密碼"
+            type="password"
+            id="passwordConfirm"
+            autoComplete="passwordConfirm"
+            size="small"
           />
           <Box className="flex flex-col items-center mb-5">
             <Typography component="p" variant="caption">
-              忘記密碼了嗎 ?{' '}
+              已經有帳號了嗎 ?{' '}
               <Link href="#" className="hover:text-secondary ml-1" variant="caption" underline="none">
-                重設密碼
+                登入
               </Link>
             </Typography>
           </Box>
 
           <Button type="submit" fullWidth variant="contained">
-            登入
+            註冊
           </Button>
         </Box>
 
@@ -73,9 +118,13 @@ const LogIn = () => {
         </Button>
         <Box className="flex flex-col items-center mb-5">
           <Typography component="p" variant="caption">
-            還沒有帳號嗎 ?{' '}
-            <Link href="#" className="hover:text-secondary ml-1" variant="caption" underline="none">
-              馬上註冊
+            註冊即表示您同意
+            <Link href="#" className="hover:text-secondary mx-1" variant="caption" underline="none">
+              使用條款
+            </Link>
+            及
+            <Link href="#" className="hover:text-secondary mx-1" variant="caption" underline="none">
+              隱私權政策
             </Link>
           </Typography>
         </Box>
@@ -83,4 +132,4 @@ const LogIn = () => {
     </Container>
   );
 };
-export default LogIn;
+export default SignUp;
