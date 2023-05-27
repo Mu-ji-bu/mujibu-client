a842ef3985dc39292e4c3bf68bbce639f0cff603
Thibetanus <steventhibe@gmail.com>
2023-05-27 20:54:53 +0800

    feat(projects路徑): 修改 projects/ 路徑

---
+++ src/pages/projects/msg-board/index.tsx
@@ -0,0 +1,11 @@
+import ProjectsLayout from '@/components/layout/ProjectsLayout';
+
+const MsgBoard = () => {
+  return (
+    <ProjectsLayout>
+      <h1>2.5 留言板</h1>
+    </ProjectsLayout>
+  );
+};
+
+export default MsgBoard;
---
+++ src/pages/projects/news/index.tsx
@@ -0,0 +1,11 @@
+import ProjectsLayout from '@/components/layout/ProjectsLayout';
+
+const News = () => {
+  return (
+    <ProjectsLayout>
+      <h1>2.3 消息更新</h1>
+    </ProjectsLayout>
+  );
+};
+
+export default News;
