19af5af95047dd5beeeef864037260c3489d433b
KaiYun Cheng <tanyacheng@gmail.com>
2023-06-16 17:50:02 +0800

    fix: 修正plan type bug

--- src/types/plan.tsx
+++ src/types/plan.tsx
@@ -1,8 +1,8 @@
-import { IProjectState } from './project';
+// import { IProjectState } from './project';
 
 export declare interface IPlanState {
   _id?: string;
-  project?: IProjectState; // 討論後刪去，暫存
+  // project?: IProjectState; // 討論後刪去，暫存
   planOrders?: any[]; // 還未建立先放any[]替代
   planName?: string;
   planType?: string;
