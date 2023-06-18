b54f05cd66e19083e1e7e124bbc3a8db5564314f
Thibetanus <steventhibe@gmail.com>
2023-06-19 01:02:33 +0800

    fix(head): head link

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -45,6 +45,7 @@ export default function Seo(props: SeoProps) {
       <title>{meta.title}</title>
       <meta name="robots" content={meta.robots} />
       <meta content={meta.description} name="description" />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
